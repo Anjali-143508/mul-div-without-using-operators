6e035f3aaeab6aa8e50d3de86add4eaf793dac51
Anjali Mahadev Kamkar <anjalikamkar69@gmail.com>
2025-04-15 11:59:01 +0530

    Add files via upload

---
+++ Mul.java
@@ -0,0 +1,16 @@
+package Pack;
+
+public class Mul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int c=a<<2;
+		int d=a>>2;
+		System.out.println(c);
+		System.out.println(d);
+
+
+	}
+
+}
